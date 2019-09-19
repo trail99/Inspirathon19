eaad53cdb2c7ff87c1ba4b8fe2114e280aa0c05b
trail99 <anup.kodlekere@gmail.com>
2019-09-20 03:47:15 +0530

    event commented

--- MyApplication1/app/src/main/java/com/example/myapplication1/Events.java
+++ MyApplication1/app/src/main/java/com/example/myapplication1/Events.java
@@ -1,63 +1,64 @@
-package com.example.myapplication1;
-
-import android.content.Intent;
-import android.widget.EditText;
-
-import androidx.fragment.app.FragmentActivity;
-
-import android.app.*;
-import com.allyants.notifyme.NotifyMe;
-import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
-import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
-import java.util.Calendar;
-
-public class Events implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
-    Calendar now = Calendar.getInstance();
-    TimePickerDialog tpd;
-    DatePickerDialog dpd;
-    EditText etText, etContent;
-    FragmentActivity ac = new FragmentActivity();
-    
-    @Override
-    public void onDateSet(DatePickerDialog view, int year, int month, int day) {
-        now.set(Calendar.YEAR, year);
-        now.set(Calendar.MONTH, month);
-        now.set(Calendar.DAY_OF_MONTH, day);
-        tpd.show(ac.getSupportFragmentManager(), "Timepickerdialog");
-    }
-
-    @Override
-    public void onTimeSet(TimePickerDialog view, int hour, int minute, int sec) {
-        now.set(Calendar.HOUR_OF_DAY, hour);
-        now.set(Calendar.MINUTE, minute);
-        now.set(Calendar.SECOND, sec);
-
-
-        NotifyMe ntfyme = new NotifyMe.Builder(ac.getApplicationContext()).color(255, 0, 0, 255).led_color(255, 255, 255, 255)
-                .time(now).addAction(new Intent(), "Snooze", false)
-                .key("test").addAction(new Intent(), "Dismiss", true, false).addAction(new Intent(), "Done")
-                .large_icon(R.mipmap.ic_launcher_round).build();
-
-    }
-}
-
-// Should take care of the date and time events of the application
-// When the period date is reached, the application should issue a notification to the user to authenticate himself/herself,
-// so as to prevent lockdown
-
-//public class Events  {
-//    private List<PeriodListener> listeners =  new ArrayList<PeriodListener>();
+//package com.example.myapplication1;
 //
-//    public void addListener(PeriodListener toAdd) {
-//        listeners.add(toAdd);
+//import android.content.Intent;
+//import android.widget.EditText;
+//
+//import androidx.fragment.app.FragmentActivity;
+//
+//import android.app.*;
+//import com.allyants.notifyme.NotifyMe;
+//
+//import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+//import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+//import java.util.Calendar;
+//
+//public class Events extends FragmentActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+//    Calendar now = Calendar.getInstance();
+//    TimePickerDialog tpd;
+//    DatePickerDialog dpd;
+//    EditText etText, etContent;
+//   // FragmentActivity ac = new FragmentActivity();
+//
+//    @Override
+//    public void onDateSet(DatePickerDialog view, int year, int month, int day) {
+//        now.set(Calendar.YEAR, year);
+//        now.set(Calendar.MONTH, month);
+//        now.set(Calendar.DAY_OF_MONTH, day);
+//        tpd.show(FragmentActivity.getSupportFragmentManager(), "Timepickerdialog");
 //    }
 //
-//    public void sayHello() {
-//        System.out.println("Hello");
+//    @Override
+//    public void onTimeSet(TimePickerDialog view, int hour, int minute, int sec) {
+//        now.set(Calendar.HOUR_OF_DAY, hour);
+//        now.set(Calendar.MINUTE, minute);
+//        now.set(Calendar.SECOND, sec);
 //
-//        for(PeriodListener pl : listeners) {
-//            pl.someOne();
-//        }
+//
+//        NotifyMe ntfyme = new NotifyMe.Builder(ac.getApplicationContext()).color(255, 0, 0, 255).led_color(255, 255, 255, 255)
+//                .time(now).addAction(new Intent(), "Snooze", false)
+//                .key("test").addAction(new Intent(), "Dismiss", true, false).addAction(new Intent(), "Done")
+//                .large_icon(R.mipmap.ic_launcher_round).build();
+//
 //    }
 //}
-
+//
+//// Should take care of the date and time events of the application
+//// When the period date is reached, the application should issue a notification to the user to authenticate himself/herself,
+//// so as to prevent lockdown
+//
+////public class Events  {
+////    private List<PeriodListener> listeners =  new ArrayList<PeriodListener>();
+////
+////    public void addListener(PeriodListener toAdd) {
+////        listeners.add(toAdd);
+////    }
+////
+////    public void sayHello() {
+////        System.out.println("Hello");
+////
+////        for(PeriodListener pl : listeners) {
+////            pl.someOne();
+////        }
+////    }
+////}
+//
--- MyApplication1/app/src/main/java/com/example/myapplication1/MainActivity.java
+++ MyApplication1/app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -2,26 +2,27 @@ package com.example.myapplication1;
 //package android.database.sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
 
-//import android.widget.Button;
-//import android.widget.EditText;
-//
-//import com.allyants.notifyme.NotifyMe;
-//import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
-//import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
-//import java.util.Calendar;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.allyants.notifyme.NotifyMe;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements  TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
     DatabaseHelper db;
-//
-//    Calendar now = Calendar.getInstance();
-//    TimePickerDialog tpd;
-//    DatePickerDialog dpd;
-//    EditText etText, etContent;
+
+    Calendar now = Calendar.getInstance();
+    TimePickerDialog tpd;
+    DatePickerDialog dpd;
+    EditText etText, etContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,42 +32,45 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    @Override
-//    public void onDateSet(DatePickerDialog view, int year, int month, int day) {
-//        now.set(Calendar.YEAR, year);
-//        now.set(Calendar.MONTH, month);
-//        now.set(Calendar.DAY_OF_MONTH, day);
-//        tpd.show(getFragmentManager(), "Timepickerdialog");
-//    }
-//
-//    @Override
-//    public void onTimeSet(TimePickerDialog view, int hour, int minute, int sec) {
-//        now.set(Calendar.HOUR_OF_DAY, hour);
-//        now.set(Calendar.MINUTE, minute);
-//        now.set(Calendar.SECOND, sec);
-//
-//
-//        NotifyMe ntfyme = new NotifyMe.Builder(getApplicationContext()).color(255, 0, 0, 255).led_color(255, 255, 255, 255)
-//                .time(now).addAction(new Intent(), "Snooze", false)
-//                .key("test").addAction(new Intent(), "Dismiss", true, false).addAction(new Intent(), "Done")
-//                .large_icon(R.mipmap.ic_launcher_round).build();
-//
-//    }
-
-    public class Events  {
-        private List<Events> listeners =  new ArrayList<Events>();
-
-        public void addListener(Events toAdd) {
-            listeners.add(toAdd);
-        }
-
-        public void sayHello() {
-            System.out.println("Hello");
-
-            for(Events pl : listeners) {
-                pl.someOne();
-            }
-        }
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int month, int day) {
+        now.set(Calendar.YEAR, year);
+        now.set(Calendar.MONTH, month);
+        now.set(Calendar.DAY_OF_MONTH, day);
+        tpd.show(getFragmentManager(), "Timepickerdialog");
     }
+
+    @Override
+    public void onTimeSet(TimePickerDialog view, int hour, int minute, int sec) {
+        now.set(Calendar.HOUR_OF_DAY, hour);
+        now.set(Calendar.MINUTE, minute);
+        now.set(Calendar.SECOND, sec);
+
+
+        NotifyMe ntfyme = new NotifyMe.Builder(getApplicationContext()).color(255, 0, 0, 255).led_color(255, 255, 255, 255)
+                .time(now).addAction(new Intent(), "Snooze", false)
+                .key("test").addAction(new Intent(), "Dismiss", true, false).addAction(new Intent(), "Done")
+                .large_icon(R.mipmap.ic_launcher_round).build();
+
+    }
+//
+//    public class Events  {
+//        private List<Events> listeners =  new ArrayList<Events>();
+//
+//        public void addListener(Events toAdd) {
+//            listeners.add(toAdd);
+//        }
+//
+//        public void sayHello() {
+//            System.out.println("Hello");
+//
+//            for(Events pl : listeners) {
+//               // pl.someOne();
+//            }
+//        }
+//    }
+//    Events e = new Events();
+//    private List<e> listeners = new ArrayList<e>();
+
 
 }
